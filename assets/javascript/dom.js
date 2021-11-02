696f800916e74b4357d1718e01433fd3cc792337
sergiogt93 <sergio.ber0556@gmail.com>
2021-11-02 09:53:57 +0100

    Fixed logs forms

--- assets/javascript/dom.js
+++ assets/javascript/dom.js
@@ -1,3 +1,4 @@
+/* Address Form */
 const ADDR_FORM = document.getElementById('addressPage')
 const FORM_FIRST_NAME =  document.getElementById('firstName')
 const ERROR_FIRST_NAME = document.getElementById("errorFirstName")
@@ -15,6 +16,8 @@ const FORM_PHONE =  document.getElementById('phone')
 const ERROR_PHONE = document.getElementById("errorPhone")
 const FORM_REGULAR_ADDR =  document.getElementById('regularAddress')
 const BTN_ADD_ADDR_FORM =  document.getElementById('regularAddress')
+
+/* User Form */
 const USER_FORM = document.getElementById("user-form")                     // const for form
 const FORM_USERNAME = document.getElementById("username")                  // const for getting username
 const ERROR_USERNAME = document.getElementById("errorUserName")
@@ -24,18 +27,6 @@ const FORM_PASSWORD = document.getElementById ("password")                 // co
 const ERROR_PASSWORD = document.getElementById("errorPassword")
 const FORM_PASSWORD_CHECK = document.getElementById ("confirm-password")   // const for getting checked password
 const ERROR_PASSWORD_CHECK = document.getElementById("errorPasswordCheck")
-const FORM_FIRSTNAME = document.getElementById("firstname")                // const for getting firstname
-const ERROR_FIRSTNAME = document.getElementById("errorFirstName")
-const FORM_LASTNAME = document.getElementById("lastname")                  // const for getting lastname
-const ERROR_LASTNAME = document.getElementById("errorLastName")
-const FORM_BIRTHDAY = document.getElementById("birthday")                 // const for getting birthday
-const ERROR_BIRTHDAY = document.getElementById("errorBirthday")
-const FORM_FIRST_ADDRESS = document.getElementById("address1")              // const for getting address1
-const ERROR_FIRST_ADDRESS = document.getElementById("errorAddress")
-const FORM_SECOND_ADDRESS = document.getElementById("address2")            // const for getting address2
-const ERROR_SECOND_ADDRESS = document.getElementById("errorAddress2")
-const FORM_POSTALCODE = document.getElementById("postalCode")            // const for getting postalcode
-const ERROR_POSTALCODE = document.getElementById("errorPostalCode")
 const BTN_ADD_USER_FORM = document.getElementById("btn-next-user")
 
 
