4d3f9f0a3d359c0027b31e0ec48df0e025cee1fa
Bertinoti <jeffersonbertinoti@gmail.com>
2021-11-02 12:46:23 +0100

    main clean

--- assets/javascript/data.js
+++
@@ -1,9 +0,0 @@
-// All data variables here
-const DIV_ARRAY= [HOME_SECTION, USER_SECTION, ADDRESS_SECTION, SHIP_SECTION, DETAIL_SECTION]
-
-const Cart= {
-    img:null,
-    color:null,
-    priceProduct:null,
-    priceShipping:null
-}
