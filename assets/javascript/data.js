6b749784264b4b98e31450a03892f8f087e34b38
Bertinoti <jeffersonbertinoti@gmail.com>
2021-11-03 09:30:50 +0100

    shippimet delivery

--- assets/javascript/data.js
+++ assets/javascript/data.js
@@ -8,6 +8,7 @@ const Cart= {
     priceShipping:null
 }
 
+monthArray=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 // solo para probar
 
